04d1d848bd8ff76147778838bc206d3e7500194b
arctignite <ijsbrand.me@gmail.com>
2018-04-01 16:30:52 +0200

    a0.0.5
    Started implementation of custom GOAP classes

---
+++ RTS/Assets/Scripts/ObjectComponents/InventoryComponent.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/**
+ * Holds resources for the Agent.
+ */
+public class InventoryComponent : MonoBehaviour
+{
+    public GameObject tool;
+    public int numLogs;
+    public int numFirewood;
+    public int numOre;
+    public string toolType = "ToolAxe";
+
+}
