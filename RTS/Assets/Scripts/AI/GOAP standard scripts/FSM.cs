79e05467c61d143ea7f85d9ee173cccce7a3f967
arctignite <ijsbrand.me@gmail.com>
2018-04-01 14:57:19 +0200

    a0.0.4
    Added GOAP standard classes

---
+++ RTS/Assets/Scripts/AI/GOAP standard scripts/FSM.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using System.Collections;
+
+/**
+ * Stack-based Finite State Machine.
+ * Push and pop states to the FSM.
+ * 
+ * States should push other states onto the stack 
+ * and pop themselves off.
+ */
+using System;
+
+
+public class FSM
+{
+
+    private Stack<FSMState> stateStack = new Stack<FSMState>(); //Stack is collection of objects, using LIFO
+
+    public delegate void FSMState(FSM fsm, GameObject gameObject);
+
+
+    public void Update(GameObject gameObject)
+    {
+        if (stateStack.Peek() != null)  //peek looks at top of stack without removing
+            stateStack.Peek().Invoke(this, gameObject);
+    }
+
+    public void pushState(FSMState state) //Pushes new state to top of the Stack
+    {
+        stateStack.Push(state);
+    }
+
+    public void popState()  //removes state from top of stack
+    {
+        stateStack.Pop();
+    }
+}
